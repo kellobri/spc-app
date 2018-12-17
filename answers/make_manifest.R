70a7616e571fa99ff1879c7d37d1bb2f5e3be41d
slopp <sean@rstudio.com>
2018-12-17 23:04:28 +0000

    dont make me copy and paste during the class

---
+++ answers/make_manifest.R
@@ -0,0 +1,6 @@
+rsconnect::writeManifest(
+  appDir = ".", 
+  appFiles = c("app.R", "config.yml", "model.RDS", "use_models.R",
+                "model_explainer.RDS", "data_preprocessor.RDS",
+                "www/rstudio.png")
+)
